dd30490056c1960d44a9379f4b6c1739ddb70364
Antonio de la Vega <antonio.v.j95@gmail.com>
2017-05-22 19:31:43 +0200

    Step 7: Making the forecast list clickable

---
+++ WeatherApp/app/src/main/java/com/example/antonio/weatherapp/ui/utils/ViewExtensions.kt
@@ -0,0 +1,11 @@
+package com.example.antonio.weatherapp.ui.utils
+
+import android.content.Context
+import android.view.View
+
+/**
+ * Created by antonio on 22/05/17.
+ */
+
+val View.ctx: Context
+    get() = context
