7d8fc663460a9beb73e0ba21c2f968719ae6904a
Antonio de la Vega <antonio.v.j95@gmail.com>
2017-05-22 18:55:54 +0200

    Step 6: Operator overloading

--- WeatherApp/app/src/main/java/com/example/antonio/weatherapp/domain/model/DomainClasses.kt
+++ WeatherApp/app/src/main/java/com/example/antonio/weatherapp/domain/model/DomainClasses.kt
@@ -4,5 +4,9 @@ package com.example.antonio.weatherapp.domain.model
  * Created by antonio on 22/05/17.
  */
 
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+    val size: Int get() = dailyForecast.size
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+}
+
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
