4b3d1136f41c4df0ea8987080239483a02c97321
Antonio de la Vega <antoniodelavegajimenez@gmail.com>
2017-05-24 00:21:12 +0200

    Step 11: Application Singleton and Delegated Properties

---
+++ WeatherApp/app/src/main/java/com/example/antonio/weatherapp/ui/App.kt
@@ -0,0 +1,20 @@
+package com.example.antonio.weatherapp.ui
+
+import android.app.Application
+import com.example.antonio.weatherapp.ui.utils.DelegatesExt
+
+/**
+ * Created by antonio on 24/05/17.
+ */
+
+class App : Application() {
+
+    companion object {
+        var instance: App by DelegatesExt.notNullSingleValue()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
